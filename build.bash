0af917e1241ae186a5aa380cffbd077e6e7b27a1
SF2005 <1208733578@qq.com>
2020-01-31 02:31:06 +0800

    update scripts:lineage_CODNAME -> exthm_CODENAME

--- build.bash
+++ build.bash
@@ -3,5 +3,5 @@ cd ${WORK_DIR}
 . build/envsetup.sh
 rm out/target/product/${TARGET_CODE_NAME}/*.zip
 rm out/target/product/${TARGET_CODE_NAME}/*.img
-lunch lineage_${TARGET_CODE_NAME}-userdebug
+lunch exthm_${TARGET_CODE_NAME}-userdebug
 mka bacon -j40
--- lunch.bash
+++ lunch.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ${WORK_DIR}
 . build/envsetup.sh
-lunch lineage_${TARGET_CODE_NAME}-userdebug
+lunch exthm_${TARGET_CODE_NAME}-userdebug
