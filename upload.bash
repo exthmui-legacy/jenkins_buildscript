49224357674f6f771ced15da892f5952fdd40816
SF2005 <1208733578@qq.com>
2020-02-09 00:31:24 +0800

    buildscript:automatic upload package to onedrive

--- upload.bash
+++ upload.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 rm -rf out
 mkdir out
-cp ${WORK_DIR}/out/target/product/${TARGET_CODE_NAME}/*.zip ./out
+
+cp ${WORK_DIR}/out/target/product/${TARGET_CODE_NAME}/lineage-*.zip ./out
+rclone purge isning-od:/rom/${TARGET_CODE_NAME}
+rclone mkdir isning-od:/rom/${TARGET_CODE_NAME}
+rclone copy out/* isning-od:/rom/${TARGET_CODE_NAME}
