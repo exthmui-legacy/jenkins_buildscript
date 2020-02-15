04012a248741447bdb5c5d933797cd76a62e50f7
SF2005 <1208733578@qq.com>
2020-02-15 15:17:58 +0800

    jenkins_buildscript:upload:lineage-*.zip -> exthm-*.zip

--- upload.bash
+++ upload.bash
@@ -2,7 +2,7 @@
 rm -rf out
 mkdir out
 
-cp ${WORK_DIR}/out/target/product/${TARGET_CODE_NAME}/lineage-*.zip ./out
+cp ${WORK_DIR}/out/target/product/${TARGET_CODE_NAME}/exthm-*.zip ./out
 rclone purge isning-od:/rom/${TARGET_CODE_NAME}
 rclone mkdir isning-od:/rom/${TARGET_CODE_NAME}
 rclone copy out/* isning-od:/rom/${TARGET_CODE_NAME}
