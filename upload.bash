2e0861a4456698d28979c3a2faf0af2ded5f1cb7
ISNing <ISNing@exthmui.cn>
2020-03-13 11:15:48 +0000

    do not upload to onedrive

--- upload.bash
+++ upload.bash
@@ -3,6 +3,6 @@ rm -rf out
 mkdir out
 
 cp ${WORK_DIR}/out/target/product/${TARGET_CODE_NAME}/exthm-*.zip ./out
-rclone purge isning-od:/rom/${TARGET_CODE_NAME}
-rclone mkdir isning-od:/rom/${TARGET_CODE_NAME}
-rclone copy out/* isning-od:/rom/${TARGET_CODE_NAME}
+#rclone purge isning-od:/rom/${TARGET_CODE_NAME}
+#rclone mkdir isning-od:/rom/${TARGET_CODE_NAME}
+#rclone copy out/* isning-od:/rom/${TARGET_CODE_NAME}
